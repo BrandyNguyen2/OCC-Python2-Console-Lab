1ae1e21132ee5d9f6f3a2f258a85ffead8bbdacf
Brandy Nguyen <134742348+BrandyNguyen2@users.noreply.github.com>
2024-04-29 21:08:48 -0700

    Delete TesterAPI.py

--- TesterAPI.py
+++
@@ -1,6 +0,0 @@
-# PROGRAMMER: Brandy Nguyen
-from WeatherReport import WeatherReport
-
-tester = WeatherReport("Costa Mesa, California", "fahrenheit", "mph")
-print(tester.currentWeatherConditions())
-print(tester.convertCurrentWeatherCondition())
